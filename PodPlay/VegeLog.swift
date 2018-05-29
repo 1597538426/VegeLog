d883cd00749d228447044d1a107b2940b5168d1d
周智伟 <zhouzhiwei@laihua.com>
2018-05-29 18:03:07 +0800

    log第一版

---
+++ PodPlay/VegeLog.swift
@@ -0,0 +1,15 @@
+//
+//  VegeLog.swift
+//  PodPlay
+//
+//  Created by 周智伟 on 2018/5/29.
+//  Copyright © 2018年 vege. All rights reserved.
+//
+
+import UIKit
+
+class VegeLog: NSObject {
+    public func startRecord() {
+        print("整起来")
+    }
+}
